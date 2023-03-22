c99c4ea2881fed00538f0ba1e221964767855f71
AlexPeti <alexandros.peteinaris@hotmail.com>
2023-03-22 12:48:08 +0200

    Configured the url and the code so it also displays the movie poster

--- movies-omdb-front-end/js/index.js
+++ movies-omdb-front-end/js/index.js
@@ -3,20 +3,21 @@ const input = document.querySelector('.search-input');
 const movieDetails = document.getElementById('movie-details');
 
 button.addEventListener('click', () => {
-    const title = input.value;
-    fetch(`http://localhost:8080/api/movies/title?title=${title}`)
-      .then(response => response.json())
-      .then(data => {
-        // Update the HTML with the movie details
-        movieDetails.innerHTML = `
-          <h2>${data.Title}</h2>
-          <p>${data.Plot}</p>
-          <p>Directed by: ${data.Director}</p>
-          <p>Starring: ${data.Actors}</p>
-        `;
-      })
-      .catch(error => {
-        // Handle any errors that occur
-        console.error(error);
-      });
-  });
+  const title = input.value;
+  fetch(`http://localhost:8080/api/movies/title?title=${title}&includePoster=true`)
+    .then(response => response.json())
+    .then(data => {
+      // Update the HTML with the movie details
+      movieDetails.innerHTML = `
+        <h2>${data.Title}</h2>
+        <img src="${data.Poster}" alt="${data.Title} poster">
+        <p>${data.Plot}</p>
+        <p>Directed by: ${data.Director}</p>
+        <p>Starring: ${data.Actors}</p>
+      `;
+    })
+    .catch(error => {
+      // Handle any errors that occur
+      console.error(error);
+    });
+});
