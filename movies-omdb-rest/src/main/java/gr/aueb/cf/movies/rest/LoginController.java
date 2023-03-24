1350c6b5a335642949356db9ecd4ae44468b8be2
AlexPeti <alexandros.peteinaris@hotmail.com>
2023-03-24 23:37:00 +0200

    Implemented a simple log in page template

--- movies-omdb-rest/src/main/java/gr/aueb/cf/movies/rest/LoginController.java
+++ movies-omdb-rest/src/main/java/gr/aueb/cf/movies/rest/LoginController.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/index")
+@Path("/login")
 public class LoginController {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
